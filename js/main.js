ed732e97eb802b984bf975cb97850da9328691e3
Mohamedا <123medoabdo@gmail.com>
2025-02-14 20:52:29 +0200

    last10

--- js/main.js
+++ js/main.js
@@ -81,7 +81,21 @@ document.addEventListener('DOMContentLoaded', function() {
         });
 
         if (isValid) {
-            alert('Form submitted successfully!');
+            const Toast = Swal.mixin({
+                toast: true,
+                position: "top-end",
+                showConfirmButton: false,
+                timer: 3000,
+                timerProgressBar: true,
+                didOpen: (toast) => {
+                toast.onmouseenter = Swal.stopTimer;
+                toast.onmouseleave = Swal.resumeTimer;
+                }
+            });
+            Toast.fire({
+                icon: "success",
+                title: "The reservation has succeeded."
+            });
             form.reset();
         }
     });
